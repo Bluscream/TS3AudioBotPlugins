9763f2d220ac73f12eefc5e63d7f23a0853f444d
Bluscream <admin@timo.de.vc>
2017-11-20 06:29:48 +0100

    hmgmgmgmemrfgergkoerg

--- ISPValidator/plugin.cs
+++ ISPValidator/plugin.cs
@@ -30,7 +30,6 @@ namespace ISPValidator {
 	public class ISPValidator : ITabPlugin {
 		private MainBot bot;
 		private Ts3FullClient lib;
-		private static FileIniDataParser iniParser;
 		private static IniData cfg;
 		private static string cfgfile;
 		private static string ispfile;
@@ -52,7 +51,8 @@ namespace ISPValidator {
 			ispfile = Path.Combine(pluginPath, "ISPs.txt");
 			lib = mainBot.QueryConnection.GetLowLibrary<Ts3FullClient>();
 			if (File.Exists(cfgfile)) {
-				cfg = iniParser.ReadFile(cfgfile);
+				var parser = new FileIniDataParser();
+				cfg = parser.ReadFile(cfgfile);
 			} else {
 				cfg = new IniData();
 				while (!Setup()) { }
@@ -67,6 +67,8 @@ namespace ISPValidator {
 			//TickPool.UnregisterTicker(Timer);
 			lib.OnClientMoved -= Lib_OnClientMoved;
 			lib.OnConnected -= Lib_OnConnected;
+			var parser = new FileIniDataParser();
+			parser.WriteFile(cfgfile, cfg);
 			PluginLog(Log.Level.Debug, $"Saved Settings to \"{cfgfile}\".");
 			PluginLog(Log.Level.Debug, "Plugin " + PluginInfo.Name + " unloaded.");
 		}
@@ -106,13 +108,27 @@ namespace ISPValidator {
 			else if (key.KeyChar == 'w')
 				cfg[section]["whitelist"] = "true";
 			else { Console.WriteLine(); return false; }
-			return true;
 			Console.Write($"{Environment.NewLine}{PluginInfo.Shortname}: Would you like to enable Debug mode? (y/n) > "); key = Console.ReadKey();
 			if (key.KeyChar == 'y')
 				cfg[section]["debug"] = "true";
 			else if (key.KeyChar == 'n')
 				cfg[section]["debug"] = "false";
 			else { Console.WriteLine(); return false; }
+			Console.WriteLine($"{PluginInfo.Shortname}: Already resolved IP's will get cached for some time if you enable this.");
+			Console.Write($"{PluginInfo.Shortname}: Clear cache interval in minutes (leave empty to disable cache) > "); line = Console.ReadLine();
+			if (String.IsNullOrWhiteSpace(line)) {
+				cfg[section]["clearcache"] = "0";
+			} else {
+				cfg[section]["clearcache"] = line;
+			}
+			Console.WriteLine($"{PluginInfo.Shortname}: Learn mode disables all actions and adds all new ISPs to the ISPs.txt until it got disabled.");
+			Console.Write($"{PluginInfo.Shortname}: Would you like to enable learn mode now? (y/n) > "); key = Console.ReadKey();
+			if (key.KeyChar == 'y')
+				cfg[section]["learn"] = "true";
+			else if (key.KeyChar == 'n')
+				cfg[section]["learn"] = "false";
+			else { Console.WriteLine(); return false; }
+			section = "Actions";
 			Console.Write($"{Environment.NewLine}{PluginInfo.Shortname}: (k)ick or (b)an infringing clients? > "); key = Console.ReadKey();
 			if (key.KeyChar == 'k')
 				cfg[section]["kickonly"] = "true";
@@ -127,25 +143,6 @@ namespace ISPValidator {
 			} else {
 				Console.WriteLine(); return false;
 			}
-			Console.Write($"{PluginInfo.Shortname}: Kick clients whoms ISP couldn't be resolved? (y/n) > "); key = Console.ReadKey();
-			if (key.KeyChar == 'y') {
-				cfg[section]["kickunknown"] = "true";
-				Console.WriteLine($"{Environment.NewLine}{PluginInfo.Shortname}: For the following 2 settings you can use {{ip}} {{isp}} {{nick}} {{clid}} {{uid}}");
-				Console.Write($"{PluginInfo.Shortname}: Poke message if unresolvable (leave empty to disable) > "); line = Console.ReadLine();
-				if (String.IsNullOrWhiteSpace(line))
-					cfg[section]["unknownpoke"] = String.Empty;
-				else {
-					cfg[section]["unknownpoke"] = line;
-				}
-				Console.Write($"{PluginInfo.Shortname}: Private message if unresolvable (leave empty to disable) > "); line = Console.ReadLine();
-				if (String.IsNullOrWhiteSpace(line))
-					cfg[section]["unknownmsg"] = String.Empty;
-				else {
-					cfg[section]["unknownmsg"] = line;
-				}
-			} else if (key.KeyChar == 'n')
-				cfg[section]["kickunknown"] = "false";
-			else { Console.WriteLine(); return false; }
 			Console.WriteLine($"{PluginInfo.Shortname}: For the following 2 settings you can use {{ip}} {{isp}} {{nick}} {{clid}} {{uid}}");
 			Console.Write($"{Environment.NewLine}{PluginInfo.Shortname}: Poke message before action is taken (leave empty to disable) > "); line = Console.ReadLine();
 			if (String.IsNullOrWhiteSpace(line))
@@ -159,16 +156,25 @@ namespace ISPValidator {
 			else {
 				cfg[section]["msg"] = line;
 			}
-			Console.WriteLine($"{PluginInfo.Shortname}: Already resolved IP's will get cached for some time if you enable this.");
-			Console.Write($"{PluginInfo.Shortname}: Clear cache interval in minutes (leave empty to disable cache) > "); line = Console.ReadLine();
-			if (!String.IsNullOrWhiteSpace(line))
-				cfg[section]["clearcache"] = line;
-			Console.WriteLine($"{PluginInfo.Shortname}: Learn mode disables all actions and adds all new ISPs to the ISPs.txt until it got disabled.");
-			Console.Write($"{PluginInfo.Shortname}: Would you like to enable learn mode now? (y/n) > "); key = Console.ReadKey();
-			if (key.KeyChar == 'y')
-				cfg[section]["learn"] = "true";
-			else if (key.KeyChar == 'n')
-				cfg[section]["learn"] = "false";
+			section = "Unresolvable";
+			Console.Write($"{PluginInfo.Shortname}: Kick clients whoms ISP couldn't be resolved? (y/n) > "); key = Console.ReadKey();
+			if (key.KeyChar == 'y') {
+				cfg[section]["enabled"] = "true";
+				Console.WriteLine($"{Environment.NewLine}{PluginInfo.Shortname}: For the following 2 settings you can use {{ip}} {{isp}} {{nick}} {{clid}} {{uid}}");
+				Console.Write($"{PluginInfo.Shortname}: Poke message if unresolvable (leave empty to disable) > "); line = Console.ReadLine();
+				if (String.IsNullOrWhiteSpace(line))
+					cfg[section]["poke"] = String.Empty;
+				else {
+					cfg[section]["poke"] = line;
+				}
+				Console.Write($"{PluginInfo.Shortname}: Private message if unresolvable (leave empty to disable) > "); line = Console.ReadLine();
+				if (String.IsNullOrWhiteSpace(line))
+					cfg[section]["msg"] = String.Empty;
+				else {
+					cfg[section]["msg"] = line;
+				}
+			} else if (key.KeyChar == 'n')
+				cfg[section]["enabled"] = "false";
 			else { Console.WriteLine(); return false; }
 			Console.WriteLine($"{Environment.NewLine}{PluginInfo.Name}: === API config ===");
 			section = "API";
@@ -184,9 +190,9 @@ namespace ISPValidator {
 				Console.WriteLine($"{Environment.NewLine}{PluginInfo.Shortname}: Default Fallback API is http://ipinfo.io/{{ip}}/org");
 				Console.Write($"{PluginInfo.Shortname}: Fallback API to use? (leave empty for default) > "); line = Console.ReadLine();
 				if (String.IsNullOrWhiteSpace(line))
-					cfg[section]["main"] = "http://ipinfo.io/{ip}/org";
+					cfg[section]["fallback"] = "http://ipinfo.io/{ip}/org";
 				else {
-					cfg[section]["main"] = line;
+					cfg[section]["fallback"] = line;
 				}
 			} else if (key.KeyChar == 'n') {
 			} else { Console.WriteLine(); return false; }
